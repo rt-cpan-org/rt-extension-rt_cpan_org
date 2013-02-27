1b7c5e57e21dc0a4bafad758fe8be66caf28dc39
Thomas Sibley <trs@bestpractical.com>
2013-02-27 13:21:15 -0800

    Keep most of rt.cpan.org's config in this extension
    Now only site-specific values like passwords and database hosts can be
    in a private RT_SiteConfig.pm.

--- etc/CPAN_Config.pm
+++ etc/CPAN_Config.pm
@@ -1,4 +1,137 @@
+# This is an RT configuration file, loaded when the RT::Extension::rt_cpan_org
+# plugin is loaded.
+
+Set( $Organization, "rt.cpan.org" );
+
+# Reminders add much complexity for not much value.
+Set( $EnableReminders, 0 );
+
 # There are too many potential owners when not limited to a specific ticket/queue.
-Set($AutocompleteOwnersForSearch, 1);
+Set( $AutocompleteOwnersForSearch, 1 );
+
+# This username format is forced and spam-protects email addresses.
+Set( $UsernameFormat, 'public' );
+
+# Email defaults; queues get their own dynamic addresses too
+Set( $CorrespondAddress, 'bugs@rt.cpan.org' );
+Set( $CommentAddress,    'comments@rt.cpan.org' );
+
+# The old $rtname used to be "cpan"; accept it
+Set( $EmailSubjectTagRegex, qr/(?:\Q$rtname\E|cpan)/i );
+
+# Too many deep links into rt.cpan.org.
+Set( $RestrictReferrer, 0 );
+
+# Don't require ModifyTicket for _both_ tickets, so bugs can be linked cross-queues.
+Set( $StrictLinkACL, 0 );
+
+# The internet expects formatting preserved and links clickable.
+Set( $PlainTextMono, 1 );
+Set( @Active_MakeClicky, qw(httpurl_overwrite) );
+
+# Fancy-pants WYSIWYG usually mangles pasted terminal and log output.
+Set( $MessageBoxRichText, 0 );
+
+# Show severity, broken in, and fixed in by default.
+Set( $DefaultSearchResultFormat, <<'EOT');
+'<a href="__WebPath__/Ticket/Display.html?id=__id__">__id__</a>/TITLE:ID',
+'<b><a href="__WebPath__/Ticket/Display.html?id=__id__">__Subject__</a></b>/TITLE:Subject',
+'__Status__',
+'__CustomField.{Severity}__',
+'<small>__LastUpdatedRelative__</small>',
+'__CustomField.{Broken in}__',
+'__CustomField.{Fixed in}__'
+EOT
+
+# Allow unauth'd access to robots.txt.
+Set( $WebNoAuthRegex, qr{(?: $WebNoAuthRegex | ^/+robots\.txt$ )}x );
+
+# Development mode should always be off for production.
+Set( $DevelMode, 0 );
+
+# Add "patched" to the default lifecycle.
+Set( %Lifecycles,
+    cpan => {
+        initial         => [ 'new' ],
+        active          => [ 'open', 'stalled', 'patched' ],
+        inactive        => [ 'resolved', 'rejected', 'deleted' ],
+
+        defaults => {
+            on_create => 'new',
+            on_merge  => 'resolved',
+            approved  => 'open',
+            denied    => 'rejected',
+            reminder_on_open     => 'open',
+            reminder_on_resolve  => 'resolved',
+        },
+
+        transitions => {
+            ''       => [qw(new open resolved)],
+
+            # from   => [ to list ],
+            new      => [qw(open stalled patched resolved rejected deleted)],
+            open     => [qw(new stalled patched resolved rejected deleted)],
+            stalled  => [qw(new open patched rejected resolved deleted)],
+            patched  => [qw(new open stalled rejected resolved deleted)],
+            resolved => [qw(new open stalled patched rejected deleted)],
+            rejected => [qw(new open stalled patched resolved deleted)],
+            deleted  => [qw(new open stalled patched rejected resolved)],
+        },
+        rights => {
+            '* -> deleted'  => 'DeleteTicket',
+            '* -> *'        => 'ModifyTicket',
+        },
+        actions => [
+            'new -> open'      => {
+                label  => 'Open It', # loc
+                update => 'Respond',
+            },
+            'new -> resolved'  => {
+                label  => 'Resolve', # loc
+                update => 'Respond',
+            },
+            'new -> rejected'  => {
+                label  => 'Reject', # loc
+                update => 'Respond',
+            },
+            'new -> deleted'   => {
+                label  => 'Delete', # loc
+            },
+
+            'open -> stalled'  => {
+                label  => 'Stall', # loc
+                update => 'Respond',
+            },
+            'open -> resolved' => {
+                label  => 'Resolve', # loc
+                update => 'Respond',
+            },
+            'open -> rejected' => {
+                label  => 'Reject', # loc
+                update => 'Respond',
+            },
+
+            'stalled -> open'  => {
+                label  => 'Open It', # loc
+            },
+            'resolved -> open' => {
+                label  => 'Re-open', # loc
+                update => 'Respond',
+            },
+            'rejected -> open' => {
+                label  => 'Re-open', # loc
+                update => 'Respond',
+            },
+            'deleted -> open'  => {
+                label  => 'Undelete', # loc
+            },
+        ],
+    },
+);
+
+# Horrible horrible hack to keep the lifecycles overriding in this config
+# instead of RT_SiteConfig.pm.  Relies on internals of RT::Config.
+my %lifecycles = RT->Config->Get("Lifecycles");
+%{ $lifecycles{default} } = %{ $lifecycles{cpan} };
 
 1;
