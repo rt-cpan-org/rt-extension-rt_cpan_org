0057111d38bf3356f7dbf4158f10767c48ec51f1
Thomas Sibley <trs@bestpractical.com>
2013-02-27 20:22:04 -0800

    Note that this bit of JS is unnecessary

--- html/NoAuth/js/rt.cpan.org.js
+++ html/NoAuth/js/rt.cpan.org.js
@@ -1,5 +1,10 @@
 jQuery(function(){
     jQuery("select[name=UpdateType] option[value=private]").remove();
+
+    /* This is redundant, thanks to the
+     * /Ticket/Elements/ShowTransaction/ModifyDisplay callback.  It doesn't
+     * hurt to leave it in place, however, and will catch if the callback stops
+     * working. */
     jQuery("#ticket-history a.comment-link").each(function(){
         var parent = this.parentNode;
         parent.removeChild(this);
