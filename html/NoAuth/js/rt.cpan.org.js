dec3fe01aee0be1b3797e39b096e7c7c9acc3fd2
Thomas Sibley <trs@bestpractical.com>
2013-03-01 17:00:32 -0800

    Attach queue autocompleter to any input with a data-autocomplete=Queues attribute
    This lets other parts of the UI indicate more inputs which should be
    autocompleted for CPAN distributions.

--- html/NoAuth/js/rt.cpan.org.js
+++ html/NoAuth/js/rt.cpan.org.js
@@ -10,4 +10,27 @@ jQuery(function(){
         parent.removeChild(this);
         parent.innerHTML = parent.innerHTML.replace(/\[\]/,"");
     });
+
+    jQuery("input[data-autocomplete=Queues]").each(function() {
+        var input = jQuery(this);
+        var opts  = {
+            source: <% RT->Config->Get('WebPath') |n,j%>
+                    + "/Helpers/Autocomplete/Queues?max=20"
+        };
+
+        if (input.attr("data-autocomplete-params") != null)
+            opts.source = opts.source + "&" + input.attr("data-autocomplete-params");
+
+        // Auto-submit once a queue is chosen
+        if (input.attr("data-autocomplete-autosubmit")) {
+            opts.select = function( event, ui ) {
+                jQuery(event.target).val(ui.item.value);
+                jQuery(event.target).closest("form").submit();
+            };
+        }
+
+        input.autocomplete(opts);
+    });
+
+    // XXX TODO: Support users as well
 });
